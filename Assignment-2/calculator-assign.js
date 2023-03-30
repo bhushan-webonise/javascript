2918fc4091ecfe11191213b7a18b2106a0128e4b
Bhushan-Gawali-webonise <bhushan.gawali@weboniselab.com>
2023-03-30 16:48:17 +0530

    assignment-js

---
+++ Assignment-2/calculator-assign.js
@@ -0,0 +1,15 @@
+function add()
+{
+  num1 = document.getElementById("n1").value;
+  num2 = document.getElementById("n2").value;
+  document.getElementById("result").innerHTML = num1 + num2;
+  console.log(result)
+}
+
+function sub() 
+{ 
+  num1 = document.getElementById("n1").value;
+  num2 = document.getElementById("n2").value;
+  document.getElementById("result").innerHTML = num1 - num2;
+}
+
